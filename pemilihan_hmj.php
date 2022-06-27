efc8d329bd2e5486c3f9f70551790087b9788677
Riandasari <107461166+Riandasari@users.noreply.github.com>
2022-06-27 13:45:49 +0700

    pemilihan_hmj
    pemilihan yang dapat di lakukan hmj sistem informasi

---
+++ pemilihan_hmj.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+    <h1>Tata Cara Pemilihan ketua hmj</h1>
+</div>
+<p>1.hanya mahasiswa sistem informasi universitas imam bonjol padang yang dapat melakukan pemilihan.</p>
+<p>2.mahasiswa harus melakukan verifikasi kepada admin untuk mendapatkan kata sandi untuk login.</p>
+<p>3.verifikasi dengan username nomor ktp.</p>
+<p>4.setelah melakukan verifikasi nomor ktp kepada admin , akan memeberi kata sandi</p>
+<p>5.setelah mendapat kata sandi pemilih dapat melakukan pemilihan terhadap calon hmj baru yang akan di pilih</p>
+<p>6.perhatikan tanggal terakhir pemilihan tersebut.</p>
+<p> sekian terima kasih atas partisipasi mahasiswa dalam acara pemilihan hmj sistem informasi uin ib padang periode 2021-2022</p>
